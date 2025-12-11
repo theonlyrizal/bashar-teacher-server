eed27d10415a7cabaa14b1b5c6f9964465708890
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-12-11 06:52:50 +0600

    crud for own tutions

--- index.js
+++ index.js
@@ -302,6 +302,20 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/api/tuitions/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tuitionsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete('/api/tuitions/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tuitionsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 });
     console.log('Pinged your deployment. You successfully connected to MongoDB!');
